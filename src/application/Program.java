c32fbebf8fa7f949d1bf79943a92169c5075e9ff
Pedro Dantas <pedrood1232@gmail.com>
2022-09-19 15:19:34 -0300

    Implementação do movimento de promoção

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,6 +34,12 @@ public class Program {
                 if (capturedPiece != null){
                     captured.add(capturedPiece);
                 }
+
+                if (chessMatch.getPromoted() != null){
+                    System.out.print("Escolha peça para promoção (Q/H/R/B): ");
+                    String type = sc.nextLine();
+                    chessMatch.replacePromotedPiece(type);
+                }
             }catch (ChessException e){
                 System.out.println(e.getMessage());
                 sc.nextLine();
